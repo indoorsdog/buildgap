febb41da1bfa453d3c6b33b32a66ca6cad135089
Luke Patterson <lukewpatterson@gmail.com>
2012-05-31 15:05:50 -0500

    delete files from old directory structure

--- shell/TestSomePython.py
+++
@@ -1,3 +0,0 @@
-import simplejson as json
-
-print json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',',':'))
--- shell/TestSomeRuby.rb
+++
@@ -1,6 +0,0 @@
-# do I need to require rubygems and bundler/setup?
-require "rubygems"
-require "bundler/setup"
-require "i18n"
-
-puts I18n.translate 'asdf'
